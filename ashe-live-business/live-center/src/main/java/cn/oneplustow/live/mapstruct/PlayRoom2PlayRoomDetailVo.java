dfe90d87390d9f460faf3eef2f5116a35c648c42
oneplustow <cc@oneplustow.cn>
2021-04-29 11:46:02 +0800

    perf(直播中心): 增加转换类
    1.增加PlayRoom到详情和分页对象的Vo转换类

---
+++ ashe-live-business/live-center/src/main/java/cn/oneplustow/live/mapstruct/PlayRoom2PlayRoomDetailVo.java
@@ -0,0 +1,19 @@
+package cn.oneplustow.live.mapstruct;
+
+import cn.hutool.core.util.IdUtil;
+import cn.oneplustow.common.constant.DbConstants;
+import cn.oneplustow.common.mapstruct.IMapStruct;
+import cn.oneplustow.live.entity.PlayRoom;
+import cn.oneplustow.live.vo.PlayRoomDetailVo;
+import cn.oneplustow.live.vo.SavePlayRoomDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+/**
+ * @author cc
+ */
+@Mapper(componentModel = "spring",imports = IdUtil.class)
+public interface PlayRoom2PlayRoomDetailVo extends IMapStruct<PlayRoom, PlayRoomDetailVo> {
+
+}
