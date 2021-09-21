b223daafcc6a8b2e4d76c6d6e3f941684a20cff2
oneplustow <cc@oneplustow.cn>
2021-09-21 10:02:56 +0800

    fix(全局): 修改所有mysql reids nacos地址
    1.修改所有mysql reids nacos地址

--- ashe-live-business/message-center/src/test/java/cn/oneplustow/SpringMvcTest.java
+++ ashe-live-business/message-center/src/test/java/cn/oneplustow/SpringMvcTest.java
@@ -1,13 +1,15 @@
 package cn.oneplustow;
 
 import cn.oneplustow.mc.MessageCenterApplication;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.util.AntPathMatcher;
 
-@SpringBootTest(classes = MessageCenterApplication.class)
+//@SpringBootTest(classes = MessageCenterApplication.class)
 public class SpringMvcTest {
 
     @Autowired
@@ -20,23 +22,23 @@ public class SpringMvcTest {
     public void test1(){
         System.out.println(environment.getProperty("cc"));
     }
-//
-//    @Test
-//    public void testProperties(){
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//        config.setPassword("password");
-//        config.setAlgorithm("PBEWithMD5AndDES");
-//        config.setKeyObtentionIterations("1000");
-//        config.setPoolSize("1");
-//        config.setProviderName("SunJCE");
-//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-//        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-//        config.setStringOutputType("base64");
-//        encryptor.setConfig(config);
-//        System.out.println(encryptor.encrypt("ashe-live"));
-//        System.out.println(encryptor.encrypt("ashe-live-prod"));
-//    }
+
+    @Test
+    public void testProperties(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("oneplustow");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        System.out.println(encryptor.encrypt("data.oneplustow.cn:8848"));
+        System.out.println(encryptor.encrypt("mysql2020920."));
+    }
 
 
 
