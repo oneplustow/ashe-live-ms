60130c70bbfe3b7dfeb96135c80a062938aa41a5
itlia <itliangwj@163.com>
2022-03-21 09:52:26 +0800

    fix(消息中心)：增加消息发送
    增加消息发送

---
+++ ashe-live-business/message-center/src/main/java/cn/oneplustow/mc/service/handler/BaseSendMsgHandle.java
@@ -0,0 +1,47 @@
+package cn.oneplustow.mc.service.handler;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.oneplustow.api.sc.model.SimpleUser;
+import cn.oneplustow.api.sc.service.UserService;
+import cn.oneplustow.mc.entity.Message;
+import cn.oneplustow.mc.vo.SendMessageVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author lwj
+ */
+@Slf4j
+public abstract class BaseSendMsgHandle implements ISendMsgHandle {
+    @Autowired
+    private UserService sysUserService;
+
+    @Override
+    public void sendMsg(SendMessageVo sendMessageVo) {
+        //将用户id转换为具体的可送发对象
+        Collection<Long> userIdList = sendMessageVo.getEsUserId();
+        List<String> esReceiver = sendMessageVo.getEsReceiver();
+
+        if(CollUtil.isNotEmpty(userIdList)){
+            Set<String> collect = userIdList.stream().map(String::valueOf).collect(Collectors.toSet());
+            List<SimpleUser> simpleUsers = sysUserService.getSimpleUserListById(collect);
+            esReceiver = simpleUsers.stream().map(this::getReceiver).collect(Collectors.toList());
+        }
+
+        for (String receiver : esReceiver) {
+             doSendMsg(receiver,sendMessageVo);
+        }
+        //保存推送记录
+        //save(resultList);
+    }
+
+
+    abstract String getReceiver(SimpleUser user);
+
+    abstract Message doSendMsg(String esReceiver, SendMessageVo sendMessageVo);
+}
