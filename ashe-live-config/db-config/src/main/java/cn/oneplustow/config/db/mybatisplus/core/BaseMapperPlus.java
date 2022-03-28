8884fc532e094d5dd3170b3bdefa8f779ae1a7ed
oneplustow <cc@oneplustow.cn>
2022-03-28 17:58:40 +0800

    perf(全局): 对整体分页逻辑和类转换逻辑进行抽象
    1.增强部分mybatisplus接口，能够在查询后直接进行类型转换，配合Struct
    2.对分页部分进行统一，全部使用mybatisplus分页方法，取代github Page

---
+++ ashe-live-config/db-config/src/main/java/cn/oneplustow/config/db/mybatisplus/core/BaseMapperPlus.java
@@ -0,0 +1,22 @@
+package cn.oneplustow.config.db.mybatisplus.core;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Collection;
+
+/**
+ * 自定义 Mapper 接口, 实现 自定义扩展
+ *
+ * @author Lion Li
+ * @since 2021-05-13
+ */
+public interface BaseMapperPlus<T> extends BaseMapper<T> {
+
+	/**
+	 * 单sql批量插入( 全量填充 无视数据库默认值 )
+	 * 适用于无脑插入
+	 */
+	int insertAll(@Param("list") Collection<T> batchList);
+
+}
---
+++ ashe-live-config/db-config/src/main/java/cn/oneplustow/config/db/mybatisplus/methods/InsertAll.java
@@ -0,0 +1,74 @@
+package cn.oneplustow.config.db.mybatisplus.methods;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import org.apache.ibatis.executor.keygen.Jdbc3KeyGenerator;
+import org.apache.ibatis.executor.keygen.KeyGenerator;
+import org.apache.ibatis.executor.keygen.NoKeyGenerator;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
+
+/**
+ * 单sql批量插入( 全量填充 无视数据库默认值 )
+ *
+ * @author Lion Li
+ */
+public class InsertAll extends AbstractMethod {
+
+	@Override
+	public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
+		final String sql = "<script>insert into %s %s values %s</script>";
+		final String fieldSql = prepareFieldSql(tableInfo);
+		final String valueSql = prepareValuesSqlForMysqlBatch(tableInfo);
+		KeyGenerator keyGenerator = new NoKeyGenerator();
+		String sqlMethod = "insertAll";
+		String keyProperty = null;
+		String keyColumn = null;
+		// 表包含主键处理逻辑,如果不包含主键当普通字段处理
+		if (StrUtil.isNotBlank(tableInfo.getKeyProperty())) {
+			if (tableInfo.getIdType() == IdType.AUTO) {
+				/** 自增主键 */
+				keyGenerator = new Jdbc3KeyGenerator();
+				keyProperty = tableInfo.getKeyProperty();
+				keyColumn = tableInfo.getKeyColumn();
+			} else {
+				if (null != tableInfo.getKeySequence()) {
+					keyGenerator = TableInfoHelper.genKeyGenerator(sqlMethod, tableInfo, builderAssistant);
+					keyProperty = tableInfo.getKeyProperty();
+					keyColumn = tableInfo.getKeyColumn();
+				}
+			}
+		}
+		final String sqlResult = String.format(sql, tableInfo.getTableName(), fieldSql, valueSql);
+		SqlSource sqlSource = languageDriver.createSqlSource(configuration, sqlResult, modelClass);
+		return this.addInsertMappedStatement(mapperClass, modelClass, sqlMethod, sqlSource, keyGenerator, keyProperty, keyColumn);
+	}
+
+	private String prepareFieldSql(TableInfo tableInfo) {
+		StringBuilder fieldSql = new StringBuilder();
+		if (StrUtil.isNotBlank(tableInfo.getKeyColumn())) {
+			fieldSql.append(tableInfo.getKeyColumn()).append(",");
+		}
+		tableInfo.getFieldList().forEach(x -> fieldSql.append(x.getColumn()).append(","));
+		fieldSql.delete(fieldSql.length() - 1, fieldSql.length());
+		fieldSql.insert(0, "(");
+		fieldSql.append(")");
+		return fieldSql.toString();
+	}
+
+	private String prepareValuesSqlForMysqlBatch(TableInfo tableInfo) {
+		final StringBuilder valueSql = new StringBuilder();
+		valueSql.append("<foreach collection=\"list\" item=\"item\" index=\"index\" open=\"(\" separator=\"),(\" close=\")\">");
+		if (StrUtil.isNotBlank(tableInfo.getKeyColumn())) {
+			valueSql.append("#{item.").append(tableInfo.getKeyProperty()).append("},");
+		}
+		tableInfo.getFieldList().forEach(x -> valueSql.append("#{item.").append(x.getProperty()).append("},"));
+		valueSql.delete(valueSql.length() - 1, valueSql.length());
+		valueSql.append("</foreach>");
+		return valueSql.toString();
+	}
+}
+
