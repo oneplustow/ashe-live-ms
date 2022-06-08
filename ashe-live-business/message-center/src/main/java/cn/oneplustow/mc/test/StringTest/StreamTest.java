975f77ce262a8dab96df2152fbac8d15b39dbb5b
itlia <itliangwj@163.com>
2022-06-08 10:43:46 +0800

    fix(消息中心)：优化算法
    数组性能测试

---
+++ ashe-live-business/message-center/src/main/java/cn/oneplustow/mc/test/StringTest/StreamTest.java
@@ -0,0 +1,18 @@
+package cn.oneplustow.mc.test.StringTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Author lwj
+ * Date 2022/6/8
+ * Description TODO
+ * BVersion 1.0
+ **/
+public class StreamTest {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("1", "2");
+
+    }
+}
